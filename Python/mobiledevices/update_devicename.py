a31cbd4a915e54b59204bc084d2fb3f0ff3c8fd6
Brandon <me@brandonusher.com>
2015-10-22 15:38:40 -0700

    Changed comments to be more accurate to this script. Guess that's what I get for copying one script to make another

--- Python/mobiledevices/update_devicename.py
+++ Python/mobiledevices/update_devicename.py
@@ -1,12 +1,8 @@
 ##
-# This is used to update device records with an asset tag based off of a CSV file
+# This is used to update device names to be the same as the username assigned to the device
 #
 # You can run this by typing the following:
-#       python update_assettags.py ~/Desktop/assets.csv
-#
-# The file does not need to be on your Desktop or called assets.csv
-# The assets.csv file must have two headers:
-#       serial_number and asset_tag
+#       python update_devicename.py
 #
 # If you have any issues, please submit an issue on this GitHub Repo and
 # I can try to help you at the next chance I get.
@@ -66,8 +62,7 @@ except Exception as e:
     print(e)
     sys.exit(0)
 
-# Simply loop through all the records in the CSV, minus the header
-# since the reader sees the first line as the reference point
+# Simply loop through all the records in the JSS
 for device in mobile_devices:
     try:
         # Get the device ID then assemble the url to fetch the rest of the device record
@@ -81,7 +76,7 @@ for device in mobile_devices:
         # Device name is the same as the username field. Get it, assemble the push URL then push it
         device_name = mobile_device['location']['username']
         deviceNameURL = '%s/mobiledevicecommands/command/DeviceName/%s/id/%s' % (jssAPIURL, device_name, device_id)
-        # This is where the request is sent to update the asset tag of the device
+        # This is where the request is sent to update the name of the device
         request = requests.post(deviceNameURL, headers=requestHeaders, verify=verifySSL, auth=(jssAPIUsername, jssAPIPassword))
 
         if request.status_code == 201:
